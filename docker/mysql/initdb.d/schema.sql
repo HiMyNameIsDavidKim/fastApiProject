81f31b9240d8a55998b9ab42523b1cbe7b968cb9
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-01-05 18:17:32 +0900

    1/5 study update

--- docker/mysql/initdb.d/schema.sql
+++ docker/mysql/initdb.d/schema.sql
@@ -1,8 +1,8 @@
 create table users(
-    id primary key = models.AutoField(primary_key=True),
-    username varchar(100) = models.CharField(max_length=100),
-    pwd varchar(255) = models.CharField(max_length=255),
-    created_at date = models.DateField(auto_now=True),
-    ranks integer = models.IntegerField(default=1),
-    points integer = models.IntegerField(default=0),
-)
+    id int(11) primary key,
+    username varchar(100),
+    pwd varchar(255),
+    created_at date,
+    ranks integer,
+    points integer
+)charset = utf8;
--- docker/mysql/initdb.d/testdata.sql
+++ docker/mysql/initdb.d/testdata.sql
@@ -1,3 +1,5 @@
 insert into users(id, username, pwd, created_at, ranks, points)
-values ("0", "홍길동", "123", "2022-12-22", "1", "0")
+values ("0", "홍길동", "123", "2022-12-22", "1", "0");
 
+insert into users(id, username, pwd, created_at, ranks, points)
+values ("1", "김길동", "123", "2022-12-22", "1", "0");
